b5c7cf528d6d0705e447f6a0b34ec27624958c07
Choon Keat <ckseet@gmail.com>
2019-11-16 20:02:15 +0800

    Fix for datetime instead of date in names of columns Addition of sample tables 	modified:   create_mysql_db.sql

--- sample_db/create_mysql_db.sql
+++ sample_db/create_mysql_db.sql
@@ -4,7 +4,12 @@ create table sample_tbi
 (
 	id		bigint not null auto_increment primary key,
 	description 	varchar(256),
-	create_date	datetime,
-	change_date	datetime,
+	create_datetime	datetime,
+	change_datetime	datetime,
 	active		tinyint
 );
+insert into sample_tbl(description, create_datetime, change_datetime, active) values ('Entry One', now(), now(), 1);
+insert into sample_tbl(description, create_datetime, change_datetime, active) values ('Entry Two', now(), now(), 1);
+insert into sample_tbl(description, create_datetime, change_datetime, active) values ('Entry Three', now(), now(), 1);
+insert into sample_tbl(description, create_datetime, change_datetime, active) values ('Entry Four', now(), now(), 1);
+insert into sample_tbl(description, create_datetime, change_datetime, active) values ('Entry Five', now(), now(), 1);
